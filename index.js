21bf63fb19ab89649e728e55584688085799b45d
hovinhthanh7893 <hovinhthanh7893@gmail.com>
2023-01-03 04:38:31 +0100

    change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.148.0";
-import {OrbitControls} from "https://cdn.skypack.dev/three@0.148.0/examples/jsm/controls/OrbitControls.js"
+import * as THREE from 'three';
+import { OrbitControls } from 'OrbitControls';
 
 //Scene
 const scene = new THREE.Scene();
