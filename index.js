8dd9f7a93a2d760de453004d656d69e4546bba36
hovinhthanh7893 <hovinhthanh7893@gmail.com>
2023-01-03 05:34:00 +0100

    change

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const sizes = {
 //Light
 const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 10, 10);
-light.intensity = 1.25;
+light.intensity = 1.1;
 scene.add(light);
 
 //Camera
