7cfc673dfffc326121f3d17ba5661373f4f3b7cd
hovinhthanh7893 <hovinhthanh7893@gmail.com>
2023-01-07 02:00:39 +0100

    change

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const sizes = {
   heith: window.innerHeight
 };
 let mousePosition = { x: 0, y: 0 };
-const fov = 45;
+const fov = 40;
 const aspectRatio = sizes.width / sizes.heith;
 const near = 1;
-const far = 10000;
+const far = 100;
 
 //Scene
 const scene = new THREE.Scene();
@@ -19,7 +19,7 @@ const scene = new THREE.Scene();
 const moonAlbedo = new THREE.TextureLoader().load('moon_Albedo.jpeg');
 const moonNormal = new THREE.TextureLoader().load('moon_Normal.jpeg');
 const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 64, 64),
+  new THREE.SphereGeometry(4, 64, 64),
   new THREE.MeshStandardMaterial({
     map: moonAlbedo,
     normalMap: moonNormal,
@@ -30,10 +30,10 @@ const moon = new THREE.Mesh(
 scene.add(moon);
 
 //Light
-const hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, 0.9);
+const hemisphereLight = new THREE.HemisphereLight(0xffffff,0x000000, 0.5);
 const globalLight = new THREE.AmbientLight(0xffffff, 0.9);
-const shadowLight = new THREE.DirectionalLight(0xffffff, 1);
-shadowLight.position.set(-30, 40, 20);
+const shadowLight = new THREE.DirectionalLight(0xff8f16, 0.4);
+shadowLight.position.set(0, 450, 350);
 shadowLight.castShadow = true;
 shadowLight.shadow.camera.left = -400;
 shadowLight.shadow.camera.right = 400;
@@ -45,15 +45,13 @@ shadowLight.shadow.mapSize.width = 2048;
 shadowLight.shadow.mapSize.height = 2048;
 scene.add(hemisphereLight, shadowLight, globalLight);
 
-const lightHelper = new THREE.DirectionalLightHelper(shadowLight);
+// const lightHelper = new THREE.DirectionalLightHelper(shadowLight);
 const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper, lightHelper)
+scene.add(gridHelper)
 
 //Camera
 const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
-camera.position.x = 0;
-camera.position.z = 200;
-camera.position.y = 100;
+camera.position.set( 40, 10, 8 );
 scene.add(camera);
 
 //Renderer
@@ -68,11 +66,12 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 //Controls
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set( 0, 0.5, 0 );
 controls.enableDamping = true;
 controls.enablePan = false;
-controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
+// controls.enableZoom = false;
+// controls.autoRotate = true;
+// controls.autoRotateSpeed = 5;
 
 //Resize
 window.addEventListener('resize', handleWindowResize, false);
@@ -87,12 +86,12 @@ function handleWindowResize() {
 }
 
 //Mouse track
-document.addEventListener('mousemove', handleMouseMove, false);
-function handleMouseMove(event) {
-  var tx = -1 + (event.clientX / sizes.width)*2;
-  var ty = 1 - (event.clientY / sizes.heith)*2;
-  mousePosition = {x:tx, y:ty};
-}
+// document.addEventListener('mousemove', handleMouseMove, false);
+// function handleMouseMove(event) {
+//   var tx = -1 + (event.clientX / sizes.width)*2;
+//   var ty = 1 - (event.clientY / sizes.heith)*2;
+//   mousePosition = {x:tx, y:ty};
+// }
 
 
 const animate = () => {
