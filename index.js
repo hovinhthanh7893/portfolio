cdb64d859c1ec1dfe25e19a278afbd5933447063
hovinhthanh7893 <hovinhthanh7893@gmail.com>
2023-01-11 17:58:47 +0100

    change

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ let intersected, touchedObject;
 function onMouseMove(event) {
   event.preventDefault();
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  mouse.y = (event.clientY / window.innerHeight) * 2 + 1;
 }
 function onMouseDown(event) {
   event.preventDefault();
